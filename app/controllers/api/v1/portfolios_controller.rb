fe5398c6533832842c96f763a0ae715824c706c8
Vijay Raju <vijaypraju@gmail.com>
2018-04-24 21:38:43 -0700

    actions added

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::PortfoliosController < ApplicationController
+
+  def index
+    @events = Event.all
+    render json: @events
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    render json: @event
+  end
+
 end
