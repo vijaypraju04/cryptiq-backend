3edd860f0216ac056ce1098c8f83d991615684b6
Vijay Raju <vijaypraju@gmail.com>
2018-04-24 21:44:48 -0700

    user create action added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    render json: @users
+  end
+  
   def index
     @users = User.all
     render json: @users
   end
 
-  
+
 end
