de58e9c995fd5584ff81257391039c3001cfa67f
Vijay Raju <vijaypraju@gmail.com>
2018-05-08 23:10:09 -0700

    session show action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
+  def show
+    if current_user
+    render json: {
+    id: current_user.id,
+    username: current_user.username
+    } end
+  end
 end
