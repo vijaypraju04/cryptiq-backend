6e3a09bd16886b6aba1fedd60e1d535438be5b0f
Vijay Raju <vijaypraju04@gmail.com>
2018-07-03 20:30:38 -0700

    fixed route resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
   namespace :v1 do
     resources :users, only: [:index, :create, :show]
-    resources :events, only: [:index, :show, :create]
+    resources :currencies, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     post '/login', to: 'sessions#create'
     get '/current_user', to: 'sessions#show'
