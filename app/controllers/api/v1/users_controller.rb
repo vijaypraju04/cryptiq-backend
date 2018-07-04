c367fcc12750b2f203493963c044f192d85cf501
Vijay Raju <vijaypraju04@gmail.com>
2018-07-03 20:23:55 -0700

    user controller show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,11 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+
 end
