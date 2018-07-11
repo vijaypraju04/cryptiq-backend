543eb45615a2efb34a9f4a7ea0e7cd65586f0f97
Vijay Raju <vijaypraju04@gmail.com>
2018-07-10 20:31:40 -0700

    creating coin portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,5 +5,12 @@ class Portfolio < ApplicationRecord
 
   def initialCoins=(params)
     coin_count = params.length
+    budget = 3000.00
+    trans_amt = budget / coin_count
+
+    params.map do |coinInfo|
+      coin = Coin.find_or_create_by(name: coinInfo[:coinName], symbol: coinInfo[:coinSymbol])
+      amount = budget / coin_count
+    end
   end
 end
