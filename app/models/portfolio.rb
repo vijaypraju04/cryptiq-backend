19c37705dacefc24849e9b500688f281fcf7ba59
Vijay Raju <vijaypraju04@gmail.com>
2018-07-10 20:29:32 -0700

    initial coin method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,8 @@ class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :portfolio_currencies
   has_many :currencies, through: :portfolio_currencies
+
+  def initialCoins=(params)
+    coin_count = params.length
+  end
 end
